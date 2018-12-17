dbaddd25202dcd809a1d235d977c6347de36e831
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2018-12-17 12:26:12 +0100

    Update app.js
    if the array is empty set the ID of element to add to 0

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ var budgetController = (function(){
     return {
         addItem: function(type, des, val){
             var newItem, ID;
-            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            if (data.allItems[type].length > 0) {
+                //take the last id and add 1
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+             } else {
+                ID = 0;
+             }
 
             if(type === 'exp'){
 
